aad45ca8bed6a2510bfb9ca94ad617bf0754b6d8
BANGWON LEE <qkddnjs31@gmail.com>
2025-05-11 20:29:58 +0900

    add : pdf 다운로드 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -8,79 +8,120 @@ import RightContent from './components/main/RightContent'
 import { Fragment } from 'react'
 import TextGothicA1BigTitle from './components/text/TextGothicA1BigTitle'
 import TextBlackHanSansContent from './components/text/TextBlackHanSansContent'
+import generatePDF, { Resolution, Margin } from 'react-to-pdf'
 
 function App() {
-  return (
-    <div className="total_block">
-      <Analytics />
-      <header>
-        <TextGothicA1BigTitle>
-          <h1 className="text_gothic_a1_big_title_inner">안녕하세요</h1>
-          <h1 className="text_gothic_a1_big_title_inner">
-            프론트엔드 개발자 이방원입니다.
-          </h1>
-        </TextGothicA1BigTitle>
-      </header>
-      <div className="header_my_info">
-        <div className="header_inner_left">
-          <TextBlackHanSansContent>
-            <span className="text_black_hansans_content_inner">
-              차트 라이브러리를 활용해 3,000개 이상의 실시간 데이터를
-              시각화하고, 성능을 개선한 경험이 있습니다. <br /> 또한, 지속적인
-              개선을 위해 고민하고 기록하는 습관을 실천하고 있습니다.
-            </span>
-            <span className="text_black_hansans_content_inner">
-              원활한 협업을 위해 짧은 내용은 구두로, <br /> 긴 내용은 텍스트로
-              정리해 동료들이 내용을 정확히 이해하고 빠르게 대응할 수 있도록
-              지원합니다.
-            </span>
-          </TextBlackHanSansContent>
-        </div>
-        <div className="header_inner_right">
-          <TextBlackHanSansContent>
-            {/* <div className="text_box"> */}
-            <div className="my_info_box">
-              <span className="my_info_box_inner">
-                이메일 :
-                <a
-                  className="click_text"
-                  href="mailto:qkddnjs31@gmail.com"
-                  target="_blank"
-                >
-                  qkddnjs31@gmail.com
-                  <span className="click_dir">click!</span>
-                </a>
-                {/* <span>qkddnjs31@gmail.com</span> */}
-              </span>
-              <span className="my_info_box_inner">
-                instagram :
-                <a
-                  className="click_text"
-                  href="https://www.instagram.com/fe_developer_wony"
-                  target="_blank"
-                >
-                  fe_devloper_wony
-                  <span className="click_dir">click!</span>
-                </a>
-              </span>
-              <span className="my_info_box_inner">
-                Git hub :
-                <a href="https://github.com/BANGWON-LEE" target="_blank">
-                  BANGWON-LEE
-                  <span className="click_dir">click!</span>
-                </a>
-              </span>
-            </div>
+  const options = {
+    method: 'save',
+    resolution: 2, // 낮추면 크기 줄어듦 (실험 필요)
+    page: {
+      format: [130, 75], // A4 가로 (mm)
+      orientation: 'landscape',
+      margin: 2,
+    },
+    canvas: {
+      mimeType: 'image/png',
+      qualityRatio: 1,
+    },
+    overrides: {
+      pdf: {
+        compress: true,
+      },
+      canvas: {
+        useCORS: true,
+        scale: 1,
+      },
+    },
+    x: 0,
+    y: 0,
+  }
 
-            {/* </div> */}
-          </TextBlackHanSansContent>
-        </div>
+  const getTargetElement = () => document.getElementById('total_box')
+  function downloadPdf() {
+    generatePDF(getTargetElement, {
+      filename: '프론트엔드_개발자_이방원.pdf',
+      ...options,
+    })
+  }
+
+  return (
+    <>
+      <div className="pdf_block">
+        <button className="pdf_block_btn" onClick={() => downloadPdf()}>
+          PDF로 저장
+        </button>
       </div>
-      <main className="main_block">
-        <LeftContent />
-        <RightContent />
-      </main>
-    </div>
+      <div className="total_block" id="total_box">
+        <header>
+          <Analytics />
+          <TextGothicA1BigTitle>
+            <h1 className="text_gothic_a1_big_title_inner">안녕하세요</h1>
+            <h1 className="text_gothic_a1_big_title_inner">
+              프론트엔드 개발자 이방원입니다.
+            </h1>
+          </TextGothicA1BigTitle>
+        </header>
+        <div className="header_my_info">
+          <div className="header_inner_left">
+            <TextBlackHanSansContent>
+              <span className="text_black_hansans_content_inner">
+                차트 라이브러리를 활용해 3,000개 이상의 실시간 데이터를
+                시각화하고, 성능을 개선한 경험이 있습니다. <br /> 또한, 지속적인
+                개선을 위해 고민하고 기록하는 습관을 실천하고 있습니다.
+              </span>
+              <span className="text_black_hansans_content_inner">
+                원활한 협업을 위해 짧은 내용은 구두로, <br /> 긴 내용은 텍스트로
+                정리해 동료들이 내용을 정확히 이해하고 빠르게 대응할 수 있도록
+                지원합니다.
+              </span>
+            </TextBlackHanSansContent>
+          </div>
+          <div className="header_inner_right">
+            <TextBlackHanSansContent>
+              {/* <div className="text_box"> */}
+              <div className="my_info_box">
+                <span className="my_info_box_inner">
+                  이메일 :
+                  <a
+                    className="click_text"
+                    href="mailto:qkddnjs31@gmail.com"
+                    target="_blank"
+                  >
+                    qkddnjs31@gmail.com
+                    <span className="click_dir">click!</span>
+                  </a>
+                  {/* <span>qkddnjs31@gmail.com</span> */}
+                </span>
+                <span className="my_info_box_inner">
+                  instagram :
+                  <a
+                    className="click_text"
+                    href="https://www.instagram.com/fe_developer_wony"
+                    target="_blank"
+                  >
+                    fe_devloper_wony
+                    <span className="click_dir">click!</span>
+                  </a>
+                </span>
+                <span className="my_info_box_inner">
+                  Git hub :
+                  <a href="https://github.com/BANGWON-LEE" target="_blank">
+                    BANGWON-LEE
+                    <span className="click_dir">click!</span>
+                  </a>
+                </span>
+              </div>
+
+              {/* </div> */}
+            </TextBlackHanSansContent>
+          </div>
+        </div>
+        <main className="main_block">
+          <LeftContent />
+          <RightContent />
+        </main>
+      </div>
+    </>
   )
 }
 
--- src/components/main/LeftContent.jsx
+++ src/components/main/LeftContent.jsx
@@ -47,25 +47,37 @@ export default function LeftContent() {
               <TextBlackHanSansContent>
                 <div className="text_box">
                   <ul className="text_box_ul">
-                    <li>
-                      기존 1~2초 주기 API 폴링 방식으로 인해 불필요한 요청을
-                      보냄. 이를 해결하기 위해 MQTT 프로토콜을 도입하여 <br />
-                      데이터 발생 시에만 실시간 수신하는 구조로 변경. 또한
-                      ApexCharts를 활용하여 초기 렌더링과 데이터 갱신을 <br />
-                      분리하여 최적화를 진행.
+                    <li className="content_li">
+                      <span>•</span>
+                      <span>
+                        기존 1~2초 주기 API 폴링 방식으로 인해 불필요한 요청을
+                        보냄. 이를 해결하기 위해 MQTT 프로토콜을 도입하여 데이터
+                        발생 시에만 실시간 수신하는 구조로 변경. 또한
+                        ApexCharts를 활용하여 초기 렌더링과 데이터 갱신을
+                        분리하여 최적화를 진행.
+                      </span>
                     </li>
-                    <li>
-                      작업 생산성을 높이기 위해 Chart.js 기반 재사용 가능한 차트
-                      컴포넌트를 개발. 차트 타입과 API 데이터만 주입하면 사용할
-                      수 있도록 설계하고, 스타일 커스터마이징 기능을 추가하여
-                      다양한 시각화 요구를 지원. 이를 통해 데이터 <br />
-                      시각화 작업의 편의성과 개발 속도 향상에 기여.
+                    <br />
+                    <li className="content_li">
+                      <span>•</span>
+                      <span>
+                        작업 생산성을 높이기 위해 Chart.js 기반 재사용 가능한
+                        차트 컴포넌트를 개발. 차트 타입과 API 데이터만 주입하면
+                        사용할 수 있도록 설계하고, 스타일 커스터마이징 기능을
+                        추가하여 다양한 시각화 요구를 지원. 이를 통해 데이터
+                        <br />
+                        시각화 작업의 편의성과 개발 속도 향상에 기여.
+                      </span>
                     </li>
-                    <li>
-                      실시간 데이터 구현에 있어서 ChartJS가 ApexChart보다 초기
-                      렌더링 속도 및 성능 향상에 이점이 있음을 파악,
-                      <br /> LCP를 <strong>21.3초 → 4.0초</strong>, FCP를
-                      <strong>2.6초 → 0.2초</strong>로 대폭 개선.
+                    <br />
+                    <li className="content_li">
+                      <span>•</span>
+                      <span>
+                        실시간 데이터 구현에 있어서 ChartJS가 ApexChart보다 초기
+                        렌더링 속도 및 성능 향상에 이점이 있음을 파악,
+                        <br /> LCP를 <strong>21.3초 → 4.0초</strong>, FCP를
+                        <strong>2.6초 → 0.2초</strong>로 대폭 개선.
+                      </span>
                     </li>
                   </ul>
                   <p className="skill_text">
@@ -93,14 +105,17 @@ export default function LeftContent() {
               <TextBlackHanSansContent>
                 <div className="text_box">
                   <ul className="text_box_ul">
-                    <li>
-                      Next.js 기반 프로젝트 초기 세팅과 폴더 구조를 설계하고,
-                      서비스 사용자 페이지(19개)와 관리자 페이지(5개) <br /> 총
-                      24개 페이지에 대해 반응형 퍼블리싱을 단독으로 진행함.
-                      관리자 페이지는 MUI 기본 컴포넌트를 활용하여 <br /> 빠르게
-                      개발하고, 기본 레이아웃과 UI를 신속하게 구성함. 퍼블리싱
-                      작업은 약 2개월 내에 완료하여 <br />
-                      개발 일정 준수에 기여함.
+                    <li className="content_li">
+                      <span>•</span>
+                      <span>
+                        Next.js 기반 프로젝트 초기 세팅과 폴더 구조를 설계하고,
+                        서비스 사용자 페이지(19개)와 관리자 페이지(5개) 총 24개
+                        페이지에 대해 반응형 퍼블리싱을 단독으로 진행함. 관리자
+                        페이지는 MUI <br />
+                        기본 컴포넌트를 활용하여 빠르게 개발하고, 기본
+                        레이아웃과 UI를 신속하게 구성함. 퍼블리싱 작업은 약
+                        2개월 내에 완료하여 개발 일정 준수에 기여함.
+                      </span>
                     </li>
                   </ul>
                   <p className="skill_text">javascript, next, scss, mui</p>
@@ -127,18 +142,25 @@ export default function LeftContent() {
               <TextBlackHanSansContent>
                 <div className="text_box">
                   <ul className="text_box_ul">
-                    <li>
-                      토토 판매점 위치 정보를 지도에 효과적으로 시각화하기 위해
-                      Kakao Map API를 활용하여, API로 전달받은 토토 <br />
-                      판매점 좌표만 필터링하여 마커로 시각화. 지도는 필요 시에만
-                      동적 로딩되도록 설정하고, <br /> 이벤트 리스너를 통해 로딩
-                      흐름 최적화.
+                    <li className="content_li">
+                      <span>•</span>
+                      <span>
+                        토토 판매점 위치 정보를 지도에 효과적으로 시각화하기
+                        위해 Kakao Map API를 활용하여, API로 전달받은 토토
+                        판매점 좌표만 필터링하여 마커로 시각화. <br /> 지도는
+                        필요 시에만 동적 로딩되도록 설정하고, 이벤트 리스너를
+                        통해 로딩 흐름 최적화.
+                      </span>
                     </li>
-                    <li>
-                      사용자가 이미지를 삽입하여 글을 작성할 수 있도록
-                      React-Quill을 활용한 이미지 업로드 기능을 개발. 업로드
-                      진행 중 로딩용 임시 이미지를 삽입하고, 완료 후 실제
-                      이미지로 교체하여 사용자 경험(UX) 최적화 구현.{' '}
+                    <br />
+                    <li className="content_li">
+                      <span>•</span>
+                      <span>
+                        사용자가 이미지를 삽입하여 글을 작성할 수 있도록
+                        React-Quill을 활용한 이미지 업로드 기능을 개발. 업로드
+                        진행 중 로딩용 임시 이미지를 삽입하고, 완료 후 실제
+                        이미지로 교체하여 사용자 경험(UX) 최적화 구현.
+                      </span>
                     </li>
                   </ul>
                   <p className="skill_text">
