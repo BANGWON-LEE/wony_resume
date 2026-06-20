624d873b423756374049bbd7313ed706749d9ff3
BANGWON LEE <qkddnjs31@gmail.com>
2026-06-20 11:52:01 +0900

    edit : 헤더 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,8 @@ function App() {
           <Analytics />
           <TextGothicA1BigTitle>
             <h1 className="text_gothic_a1_big_title_inner">
-              데이터 시각화를 연구하는, 이방원
+              실시간 데이터를 시각화하고 성능을 개선하는 프론트엔드 개발자,
+              이방원
             </h1>
             {/* <h1 className="text_gothic_a1_big_title_inner">
               프론트엔드 개발자 이방원입니다.
